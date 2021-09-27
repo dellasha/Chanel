edaf8638537b2aa906ac4f73618c85a44237ae2c
dellasha <90582556+dellasha@users.noreply.github.com>
2021-09-27 13:55:20 +0900

    최초업로드

---
+++ js/sb_script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){ //시작
+
+  var swiper1 = new Swiper(".mySwiper03", {
+    navigation: {
+      nextEl: ".mySwiper03 .swiper-button-next",
+      prevEl: ".mySwiper03 .swiper-button-prev",
+    },
+  });
+
+
+
+
+
+
+});//끝
---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){ //시작
+
+  var swiper1 = new Swiper(".mySwiper01", {
+    navigation: {
+      nextEl: ".mySwiper01 .swiper-button-next",
+      prevEl: ".mySwiper01 .swiper-button-prev",
+    },
+  });
+
+  var swiper2 = new Swiper(".mySwiper02", {
+    navigation: {
+      nextEl: ".mySwiper02 .swiper-button-next",
+      prevEl: ".mySwiper02 .swiper-button-prev",
+    },
+  });
+
+
+
+
+});//끝
+
+
+
+
+
